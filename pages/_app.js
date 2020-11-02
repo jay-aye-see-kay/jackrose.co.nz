91344961402a5c701ff5e9d7aabf0b8bcb21e318
Jack Rose <user@jackrose.co.nz>
2020-11-02 11:19:44 +1100

    quick tailwind setup (not purgecss yet)

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
-import '../styles/globals.css'
+import "../css/bundle.css";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function ({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
-
-export default MyApp
