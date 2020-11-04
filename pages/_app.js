74fc744c72e8c011e603d8e35c7e911c8fb7ed65
Jack Rose <user@jackrose.co.nz>
2020-11-04 12:26:54 +1100

    setup fontawesome icons

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,6 @@
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
+
 import "../css/global.tailwind.css";
 
 export default function App({ Component, pageProps }) {
