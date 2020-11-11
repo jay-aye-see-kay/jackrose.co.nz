874cfab2ea0e31448eeaebc70f9aafb1e17de7ce
Jack Rose <user@jackrose.co.nz>
2020-11-11 13:21:58 +1100

    remove favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head />
+        <link rel="icon" href="data:," />
         <body>
           <Main />
           <NextScript />
