1e7347987f115ff66c17c2fc1e6a0a6d47d5190a
Jack Rose <user@jackrose.co.nz>
2020-11-11 15:48:14 +1100

    restore page accidentally deleted

---
+++ pages/blog-archive.tsx
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import { GetStaticProps } from "next";
+
+import { DefaultLayout } from "../components/Layouts";
+import { BlogList } from "../components/BlogList";
+import { getAllPosts } from "../lib/content";
+import { Post } from "../lib/codecs";
+
+type Props = {
+  posts: Post[];
+};
+
+const Posts = ({ posts }: Props) => {
+  return (
+    <>
+      <Head>
+        <title>Archived Posts</title>
+      </Head>
+      <DefaultLayout>
+        <div className="page-content">
+          <h1 className="page-title">Blog posts</h1>
+          <BlogList
+            posts={posts}
+            lastLink={{ label: "« Back to posts", href: "/blog" }}
+          />
+        </div>
+      </DefaultLayout>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const allPosts = getAllPosts();
+  const posts = allPosts.filter((post) => post.archived && !post.draft);
+  return {
+    props: { posts },
+  };
+};
+
+export default Posts;
