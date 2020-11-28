3b13c727931b790a07e015e54791f5b31181656a
Jack Rose <user@jackrose.co.nz>
2020-11-28 14:10:59 +1100

    add goatcounter analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,12 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          // TODO whitelist this tag in the remove js script
+          <script
+            data-goatcounter="https://jackrose.goatcounter.com/count"
+            async
+            src="//gc.zgo.at/count.js"
+          />
         </body>
       </Html>
     );
