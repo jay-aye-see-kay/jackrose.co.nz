b7dd3ea97034d8f722a10f88e1cf6bd9b9a3a858
Jack Rose <user@jackrose.co.nz>
2020-11-29 16:41:58 +1100

    fix self-link in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ export const Footer = () => {
       <hr className="m-2 mb-0"/>
       <footer id="footer">
         <div className="p-2 text-sm text-center">
-          &copy; <a href="jackrose.co.nz">Jack Rose</a> 2017 - {currentYear} //
+          &copy; <a href="https://jackrose.co.nz">Jack Rose</a> 2017 - {currentYear} //
           All content available under{" "}
           <a href="https://creativecommons.org/licenses/by/4.0/">cc-by 4.0</a>
         </div>
