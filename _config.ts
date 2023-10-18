baf1defb9a7cdcb95518c2b9f03aafc0ecf22d33
Jack Rose <user@jackrose.co.nz>
2023-10-18 21:30:20 +1100

    fix TIL index page
    - don't list self in til pages by moving out of dir
    - put slug as link text instead of url
    - add date

--- _config.ts
+++ _config.ts
@@ -1,13 +1,15 @@
 import lume from "lume/mod.ts";
 import codeHighlight from "lume/plugins/code_highlight.ts";
-import postcss from "lume/plugins/postcss.ts";
+import date from "lume/plugins/date.ts";
 import markdownTaskLists from "npm:markdown-it-task-lists";
+import postcss from "lume/plugins/postcss.ts";
 
 const site = lume({ src: "./src" });
 
 site.hooks.addMarkdownItPlugin(markdownTaskLists);
 
 site.use(codeHighlight());
+site.use(date());
 site.use(postcss());
 
 site.remoteFile(
